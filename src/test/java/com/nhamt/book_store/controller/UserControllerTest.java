cd6ba2e72e3f0283fa2e55084f9ad1d2b7100e5e
Nha MT <nhamt0306@gmail.com>
2024-11-20 01:21:04 +0700

    unit test: isolation

--- src/test/java/com/nhamt/book_store/controller/UserControllerTest.java
+++ src/test/java/com/nhamt/book_store/controller/UserControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -28,6 +29,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
     //Inject needed
     @Autowired
--- src/test/java/com/nhamt/book_store/service/UserServiceTest.java
+++ src/test/java/com/nhamt/book_store/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 
@@ -18,6 +19,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
     @Autowired
     UserService userService;
